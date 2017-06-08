0ff68a19630b6dfe495f8c3b8a785e8425611aea
Hamish Allan <hamish@gmail.com>
2017-06-08 12:58:45 -0700

    Initial Commit

---
+++ TokenTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TokenTextView
+//
+//  Created by Hamish Allan on 08/06/2017.
+//  Copyright © 2017 Olive Toast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TokenTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TokenTextView
+//
+//  Created by Hamish Allan on 08/06/2017.
+//  Copyright © 2017 Olive Toast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
